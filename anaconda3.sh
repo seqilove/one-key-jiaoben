1db867eca61abb4e8cbb89177257906d67783793
xiaofd <568032134@qq.com>
2017-05-14 19:26:53 +0800

    rename to anaconda3-install.sh

--- anaconda3.sh
+++ anaconda3.sh
@@ -2,9 +2,9 @@
 apt update && apt install -y wget bzip2
 ANACONDA_URL="https://repo.continuum.io/archive/"
 ANACONDA_VERSION=`wget -qO- $ANACONDA_URL | grep -o '"Anaconda3.*-Linux-x86_64.sh"' | cut -d '"' -f 2 | sort -r | head -n 1`
-wget $ANACONDA_URL$ANACONDA_VERSION -O anaconda3.sh
+wget $ANACONDA_URL$ANACONDA_VERSION -O anaconda3-install.sh
 echo 'export PATH=/opt/conda/bin:$PATH' > /etc/profile.d/conda.sh
-/bin/bash anaconda3.sh -b -p /opt/conda
-rm -f anaconda3.sh
+/bin/bash anaconda3-install.sh -b -p /opt/conda
+rm -f anaconda3-install.sh
 #export PATH=/opt/conda/bin:$PATH
 #source /etc/profile
