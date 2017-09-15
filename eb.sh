a5b1e62a4bf4e5a59eefcef0dd0420f89787c7a3
xiaofd <568032134@qq.com>
2017-09-15 19:03:32 +0800

    Update eb.sh

--- eb.sh
+++ eb.sh
@@ -2,7 +2,10 @@
 x=$1
 echo $x
 
-echo "15 * * * * pkill firefox-esr && firefox-esr --display=localhost:1.0 --new-tab http://www.ebesucher.com/surfbar/ebcccac.$x >/dev/null 2>&1 &" > /var/spool/cron/root
+sed -i "2s#.*#firefox-esr --display=localhost:1.0 --new-tab http://www.ebesucher.com/surfbar/ebcccac.$x >/dev/null 2>\&1 \&#" /etc/rc.local
+echo "15 * * * * reboot" > /var/spool/cron/root
+#echo "15 * * * * pkill firefox-esr && firefox-esr --display=localhost:1.0 --new-tab http://www.ebesucher.com/surfbar/ebcccac.$x >/dev/null 2>&1 &" > /var/spool/cron/root
 crontab /var/spool/cron/root
 wget ftp://192.99.11.204/ebesucher/install.sh && chmod 777 install.sh && ./install.sh;
+reboot
 firefox-esr --display=localhost:1.0 --new-tab http://www.ebesucher.com/surfbar/ebcccac.$x >/dev/null 2>&1 &
