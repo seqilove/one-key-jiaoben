57826571c69891804214dd2c3da341ea6f76d600
xiaofd <568032134@qq.com>
2017-09-15 21:05:45 +0800

    Update eb.sh

--- eb.sh
+++ eb.sh
@@ -2,7 +2,12 @@
 x=$1
 echo $x
 
-sed -i "13s#.*#/root/setpwd1.sh\&  \&\& sleep 10 \&\& /usr/bin/firefox-esr --display=localhost:1.0 --new-tab http://www.ebesucher.com/surfbar/ebcccac.$x >/dev/null 2>\&1 \&#" /etc/rc.local
+sed -i "13s#.*#/root/setpwd1.sh\&#" /etc/rc.local
+sed -i "14s#.*#sleep 10#" /etc/rc.local
+sed -i "15s#.*#/usr/bin/firefox-esr --display=localhost:1.0 --new-tab http://www.ebesucher.com/surfbar/ebcccac.$x >/dev/null 2>\&1 \&#" /etc/rc.local
+sed -i "16s#.*#exit 0#" /etc/rc.local
+#sed -i "13s#.*#/root/setpwd1.sh\&  \&\& sleep 10 \&\& /usr/bin/firefox-esr --display=localhost:1.0 --new-tab http://www.ebesucher.com/surfbar/ebcccac.$x >/dev/null 2>\&1 \&#" /etc/rc.local
+
 echo "15 * * * * reboot" > /var/spool/cron/root
 #echo "15 * * * * pkill firefox-esr && firefox-esr --display=localhost:1.0 --new-tab http://www.ebesucher.com/surfbar/ebcccac.$x >/dev/null 2>&1 &" > /var/spool/cron/root
 crontab /var/spool/cron/root
