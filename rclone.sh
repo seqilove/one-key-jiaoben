8256ed8de692601c9134bf137bc774494700cc52
xiaofd <568032134@qq.com>
2017-10-22 01:34:55 +0800

    Create rclone.sh

---
+++ rclone.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt install -y unzip
+wget https://downloads.rclone.org/rclone-current-linux-amd64.zip
+unzip rclone-current-linux-amd64.zip
+chmod 0777 ./rclone-*/rclone
+cp ./rclone-*/rclone /usr/bin/
+rm -rf ./rclone-*
