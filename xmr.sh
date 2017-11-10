caee1c19c2ff97bc69a4219138ec0657f51dddc2
xiaofd <568032134@qq.com>
2017-11-11 02:32:31 +0800

    Create xmr.sh

---
+++ xmr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+apt-get install -y python-software-properties
+add-apt-repository ppa:ubuntu-toolchain-r/test
+apt update
+apt install -y gcc-5 g++-5 make curl git
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 1 --slave /usr/bin/g++ g++ /usr/bin/g++-5
+curl -L http://www.cmake.org/files/v3.4/cmake-3.4.1.tar.gz | tar -xvzf - -C /tmp/
+cd /tmp/cmake-3.4.1/ && ./configure && make && sudo make install && cd -
+update-alternatives --install /usr/bin/cmake cmake /usr/local/bin/cmake 1 --force
+apt install -y libmicrohttpd-dev libssl-dev libhwloc-dev
+git clone https://github.com/fireice-uk/xmr-stak-cpu xmr-stak
+cd xmr-stak
+cmake .
+make install
